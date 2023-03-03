8cbf3b289ba842cc7cb28fc399ac75e82ee7e31e
dudasdaily <66531025+dudasdaily@users.noreply.github.com>
2023-03-03 23:03:27 +0900

    Add files via upload

---
+++ 20230303_for문연습.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+
+int main()
+{
+    //for문으로 별 출력
+    /*
+    char star[11] = {' ', }; // 이거 안하고 실행하면 n+1번째 초기화가 안되있어서 무작위 값이 출력됨
+
+    for(int i = 1; i <= 10; i++)
+    {
+        star[i - 1] = '*';
+        printf("%s\n", star);
+        //printf("%d\n", star[i]);
+    }
+    */
+   
+   //1부터 n까지 자연수의 합
+   int n, sum = 0;
+
+   printf("자연수 입력 : ");
+   scanf("%d", &n);
+
+   for(int i = 1; i <= n; i++)
+   {
+       sum += i;
+   }
+
+   printf("1부터 %d까지의 합 : %d\n", n, sum);
+
+   return 0;
+}
+
+
