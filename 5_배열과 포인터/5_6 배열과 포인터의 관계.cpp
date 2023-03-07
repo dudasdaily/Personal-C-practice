c77e2de832a72d0bdd77d84ae7eab440d20d2bf4
Guest <guest-jjgz9l@h27961.sgb.skir.kr>
2023-03-07 23:02:30 +0900

    .

--- 5_배열과 포인터/5_6 배열과 포인터의 관계.cpp
+++ 5_배열과 포인터/5_6 배열과 포인터의 관계.cpp
@@ -9,17 +9,30 @@ int main()
     printf("(&a) + 2 : %d\n", (&a) + 2); // 108
 
     //포인터에 정숫값 더하기
-    int* ptr = &a;
+    int* ptr = &a; //포인터를 선언할 때 가리키는 변수의 타입을 적어주는 이유이기도 함!
     printf("ptr : %d\n", ptr);
     printf("ptr +  1 : %d\n", ptr + 1);
-    printf("ptr + 11 : %d\n", ptr + 11);
+    printf("ptr + 2 : %d\n", ptr + 2);
 
     //배열의 n번째 칸의 주솟값 구하기
     int arr[5] = { 1, 2, 3, 4, 5 };
     for(int i = 0; i < 5; i++)
-        printf("%d ", &arr[i]); // 4씩 차이가 날 것임
-    
-    printf("\n");
+        printf("%d ", &arr[i]); // 4씩 차이가 날 것임 arr[i] 의 주솟값 == arr[0] + i;
+    //배열명(arr)은 배열의 0번째 칸의 주소와 같음!
+
+    //배열의 이름과 배열의 0번 칸의 주솟값
+    printf("%d %d", &arr, &arr[0]);
+
+    //포인터에 배열의 이름 집어넣기
+    int* ptr_a = arr;
+
+    printf("%d\n", &arr);
+    printf("%d\n", ptr);
+    printf("%d\n", arr[3]);
+    printf("%d\n", *(arr + 3));
+    printf("%d\n", *(ptr + 3));
+    printf("%d\n", ptr[3]);
+
 
 
     return 0;
